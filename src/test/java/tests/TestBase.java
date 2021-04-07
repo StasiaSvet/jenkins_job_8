0ae2fd72b9f7f97b25b8d51ba01ea873ff0d694d
StessyT <wildorhid@gmail.com>
2021-04-07 22:08:13 +0300

    added remote driver support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static tests.helpers.AttachmentHelper.*;
@@ -11,11 +13,11 @@ public class TestBase extends TestData {
     @BeforeAll
     static void setup() {
         addListener("AllureSelenide", new AllureSelenide());
-/*       DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/"; */
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
